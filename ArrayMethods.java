63cc5b63371effff60a1cb71c155bafee6b2c9ac
jayachristian29 <jayachristianlopez@gmail.com>
2026-05-18 05:30:06 +0000

    updated arraymethods class

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,5 +1,5 @@
 /********************************************
-*	AUTHOR:	<name>
+*	AUTHOR:	jaya eckert
 * COLLABORATORS: <names>
 *	COURSE:	CS 111 Intro to CS I - Java
 *	LAST MODIFIED: <date>
@@ -12,7 +12,12 @@
 *	Collection of useful methods for int arrays.
 *********************************************
 *	ALGORITHM:
-*	TODO: <Pseudocode for selection sort here>
+*	FOR each index i in the array from 0 to length - 2:
+* 1. Find the index of the minimum element (minIndex) in the remaining
+* unsorted portion of the arrar (from i to end) using indexOfMin.
+* 2. Swap the element at current index i with the element at minIndex
+* using the swap method.
+* 3. Repeat until the entire array is sorted.
 *********************************************
 
 /* UML CLASS DIAGRAM:
@@ -28,36 +33,63 @@ ArrayMethods
 -----------------------------------------
 */
 
+
 public class ArrayMethods
 {
+
   /**DESCRIPTION: */
   public static String arrayString(int[] a)
   {
-    return ""; //STUB to keep compiler happy
+    String result = "{ ";
+    for (int i = 0; i < a.length; i++) {
+      result += a[i];
+      if (i < a.length - 1 ){
+        result += ", ";
+      }
+    }
+    return result += " }"; //STUB to keep compiler happy
   }
   
   /**DESCRIPTION: */
   public static void swap(int[] array, int a, int b)
   {
-  
-  }
+   int temp = array[a]; 
+   array [a] = array[b];
+   array [b] = temp;
+   }
 	
 	/**DESCRIPTION: */
   public static int indexOfMin(int[] array, int startIndex)
   {
-    return 0; //STUB to keep compiler happy
+    int minIndex = startIndex;
+
+   for (int i = startIndex + 1; i < array.length; i++){
+    if (array[i] < array [minIndex]){
+      //minValue= array[i];
+      minIndex = i;
+     }
+    }
+  return minIndex;
   }
+
 
 	/**DESCRIPTION: */
   public static void reverse(int[] array)
   {
 
+    for (int i = 0; i < array.length / 2; i++)
+      {
+      swap(array, i, array.length - 1 - i);
+    }
   }
 
 	/**DESCRIPTION: */
   public static void selectionSort(int[] array)
   {
-
-  }
-
+    for (int i = 0; i < array.length - 1; i++){
+      int minPos = indexOfMin(array, i);
+     // indexOfMin = i;
+      swap(array, i, minPos);
+    }
+  } 
 }
